e94524bb56648994e492ba61b03cc7e07c057536
DevHyperCoder <arduinoleo88@gmail.com>
2021-01-24 20:04:24 +0530

    Add Insert Mode Handler

--- freelance_manager/screen.py
+++ freelance_manager/screen.py
@@ -3,6 +3,18 @@ from freelance_manager.constants import freelance_manager_welcome, exit_help
 import traceback
 
 
+def status_bar(screen, mode: str):
+    screen.addstr(0, 0, freelance_manager_welcome)
+    screen.addstr(curses.LINES - 1, 0, mode, curses.A_BOLD)
+
+
+def multi_text(screen, text: list[str], offset: int):
+    for i, text_line in enumerate(text):
+        screen.addstr(offset + i, 0, text_line)
+
+    return len(text)
+
+
 def init():
     # curses init
     screen = curses.initscr()
@@ -12,21 +24,62 @@ def init():
 
     # wide try catch so term doesnt get messed up
     try:
-        key = 0
+        mode = "LIST"
         while True:
+
+            if mode == "INSERT":
+                screen.clear()
+
+                status_bar(screen, mode)
+
+                curses.echo()
+
+                screen.addstr(1, 0, "Client: ")
+                client = screen.getstr()
+                screen.addstr(2, 0, "Language: ")
+                language = screen.getstr()
+                screen.addstr(3, 0, "Price: ")
+                price = screen.getstr()
+                screen.addstr(4, 0, "Time: ")
+                time = screen.getstr()
+
+                screen.clear()
+
+                status_bar(screen, mode)
+
+                multi_text_offset = multi_text(screen, [
+                    "Review Entry:",
+                    client,
+                    language,
+                    price,
+                    time,
+                    "",
+                    "Confirm"
+                ], 1)
+
+                if screen.getch() != ord("y"):
+                    screen.addstr(
+                        1 + multi_text_offset, 0,
+                        "Cancel")
+                    mode = "LIST"
+                    continue
+
+                mode = "LIST"
+
+            screen.clear()
+            status_bar(screen, mode)
+
+            screen.refresh()
+
+            key = screen.getch()
 
             # Break with q
             if key == ord("q"):
                 break
 
-            screen.clear()
-
-            screen.addstr(0, 0, freelance_manager_welcome, curses.A_BOLD)
-            screen.addstr(curses.LINES - 1, 0, exit_help)
-
-            screen.refresh()
-
-            key = screen.getch()
+            if key == ord("a"):
+                screen.addstr(0, 0, "asdfasdf")
+                mode = "INSERT"
 
     except Exception:
         # Show traceback
